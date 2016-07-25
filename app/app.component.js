2dfcd007799a332e2c6e0f95f0f068c1116d9e29
Shashank Agrawal <shagrawal99@gmail.com>
2016-07-25 15:06:07 +0530

    Sample Angular2 app from the quickstart guide.

---
+++ app/app.component.js
@@ -0,0 +1,15 @@
+/**
+ * Created by shashank on 25/07/16.
+ */
+
+(function (app) {
+    app.AppComponent =
+        ng.core.Component({
+            selector: 'my-app',
+            template: '<h1>My First Angular 2 App</h1>'
+        })
+            .Class({
+                constructor: function () {
+                }
+            });
+})(window.app || (window.app = {}));
